0d37f0860d8241e1b72f5b9878c5d7df23840f6e
shiningKyung <nkyou15@gmail.com>
2024-01-25 13:20:52 +0900

    Fix: 채팅 스크롤로 불러온 데이터들이 내림차순으로 보일 수 있도록 수정

--- frontend/src/components/Chat/ChatBubble.tsx
+++ frontend/src/components/Chat/ChatBubble.tsx
@@ -19,6 +19,7 @@ const ChatBubble = ({ messages, teamId, myTeamMemberId }: any) => {
       `/team/${teamId}/chat/list?date=${oneDayAgo}`,
     );
     setItems([...items, ...response.data]);
+    // setItems(items.reverse());
     setPage(page + 1);
     console.log("라이브러리 데이터 패치 >", response.data);
     console.log("채팅 데이터 타입 >", typeof response.data);
@@ -38,45 +39,52 @@ const ChatBubble = ({ messages, teamId, myTeamMemberId }: any) => {
         isReverse={true}
         useWindow={false}
       >
-        {items.map((content: any, index: any) => {
-          const { writerId, message, createdDt }: any = content;
-          const isMe = writerId === myTeamMemberId;
-          return (
-            <BubbleContainerDiv key={index} isMe={isMe}>
-              {/* <div key={index} className="flex items-start gap-2.5"> */}
-              {!isMe && (
-                <img className="w-8 h-8 rounded-full" src="" alt="chat image" />
-              )}
-              <div className="flex flex-col gap-1 w-56">
-                <div className="flex items-center space-x-2 rtl:space-x-reverse">
-                  <span className="text-sm font-semibold text-gray-900">
-                    {writerId}
-                  </span>
-                  <span className="text-sm font-normal text-gray-500">
-                    {/* createdDt.split(":00")[0].replace("T", " ").slice(0, -6)} */}
-                    {createdDt.split(":00")[0].replace("T", " ").slice(0, -6)}
-                  </span>
-                </div>
-                {isMe ? (
-                  <div className="flex flex-col justify-end leading-1.5 p-4 border-gray-200 bg-yellow-100 rounded-l-xl rounded-br-xl">
-                    <p className="text-sm font-normal text-gray-900">
-                      {" "}
-                      {message}
-                    </p>
-                  </div>
-                ) : (
-                  <div className="flex flex-col leading-1.5 p-4 border-gray-200 bg-white rounded-e-xl rounded-es-xl">
-                    <p className="text-sm font-normal text-gray-900">
-                      {" "}
-                      {message}
-                    </p>
-                  </div>
+        {items
+          .slice(0)
+          .reverse()
+          .map((content: any, index: any) => {
+            const { writerId, message, createdDt }: any = content;
+            const isMe = writerId === myTeamMemberId;
+            return (
+              <BubbleContainerDiv key={index} isMe={isMe}>
+                {/* <div key={index} className="flex items-start gap-2.5"> */}
+                {!isMe && (
+                  <img
+                    className="w-8 h-8 rounded-full"
+                    src=""
+                    alt="chat image"
+                  />
                 )}
-              </div>
-              {/* </div> */}
-            </BubbleContainerDiv>
-          );
-        })}
+                <div className="flex flex-col gap-1 w-56">
+                  <div className="flex items-center space-x-2 rtl:space-x-reverse">
+                    <span className="text-sm font-semibold text-gray-900">
+                      {writerId}
+                    </span>
+                    <span className="text-sm font-normal text-gray-500">
+                      {/* createdDt.split(":00")[0].replace("T", " ").slice(0, -6)} */}
+                      {createdDt.split(":00")[0].replace("T", " ").slice(0, -6)}
+                    </span>
+                  </div>
+                  {isMe ? (
+                    <div className="flex flex-col justify-end leading-1.5 p-4 border-gray-200 bg-yellow-100 rounded-l-xl rounded-br-xl">
+                      <p className="text-sm font-normal text-gray-900">
+                        {" "}
+                        {message}
+                      </p>
+                    </div>
+                  ) : (
+                    <div className="flex flex-col leading-1.5 p-4 border-gray-200 bg-white rounded-e-xl rounded-es-xl">
+                      <p className="text-sm font-normal text-gray-900">
+                        {" "}
+                        {message}
+                      </p>
+                    </div>
+                  )}
+                </div>
+                {/* </div> */}
+              </BubbleContainerDiv>
+            );
+          })}
       </InfiniteScroll>
       {messages.map((content: any, index: any) => {
         const { writerId, message, createdDt }: any = content;
